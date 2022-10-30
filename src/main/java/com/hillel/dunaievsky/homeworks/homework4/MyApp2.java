58b21a0a663e06acd795f50423749e965f1c328c
Dunaievsky Vladyslav <vladunaevsky@gmal.com>
2022-10-30 11:13:03 +0300

    commit Chinese dynasty

--- src/main/java/com/hillel/dunaievsky/homeworks/homework4/MyApp2.java
+++ src/main/java/com/hillel/dunaievsky/homeworks/homework4/MyApp2.java
@@ -2,13 +2,62 @@ package com.hillel.dunaievsky.homeworks.homework4;
 
 public class MyApp2{
 
-      public static void main(String[]args){
+
+    public static void main(String[] args) {
+
+
+        int aLe = 13;
+        int bLe = 24;
+        int cLe = 46;
+        int allWarriors = 860;
+
+        int aWarriors = aLe * allWarriors;
+        int bWarriors = bLe * allWarriors;
+        int cWarriors = cLe * allWarriors;
+
+        int totalDamage = aWarriors + bWarriors + cWarriors;
+        System.out.println(totalDamage);
+
+        int aMin = 9;
+        int bMin = 35;
+        int cMin = 12;
+        double allWarriors2 = (int) 860 * 1.5;
+
+        int aWarriors2 = (int) ((int) aMin * allWarriors2);
+        int bWarriors2 = (int) ((int) bMin * allWarriors2);
+        int cWarriors2 = (int) ((int) cMin * allWarriors2);
+
+        int totalDamage2 = aWarriors2 + bWarriors2 + cWarriors2;
+
+        System.out.println(totalDamage2);
 
 
 
+
+
+
+
+
+        {
         }
 
-     }
+
+
+
+
+        {
+        }
+
+
+
+
+    }
+    }
+
+
+
+
+
 
 
 
