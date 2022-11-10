0902c9e6e329a6a6e7c3ac4935253f89e0a417a6
Dunaievsky Vladyslav <vladunaevsky@gmal.com>
2022-11-10 15:32:42 +0300

    commit GuessTheNumber

---
+++ src/main/java/com/hillel/dunaievsky/homeworks/homework6/MyApp6.java
@@ -0,0 +1,40 @@
+package com.hillel.dunaievsky.homeworks.homework6;
+
+import java.util.Scanner;
+
+public class MyApp6 {
+
+     public static void main(String[] args) {
+          System.out.println("Вгадай число, яке 'загадав' комп'ютер. Випробуй свою вдачу!");
+          int random = (int) (Math.random() * 11);
+
+          Scanner sc = new Scanner(System.in);
+
+          System.out.println("Введіть число від 0 до 10: ");
+
+          for (; ; ) {
+               if (sc.hasNextInt()) {
+                    int num = sc.nextInt();
+                    if (num < 0 || num > 10) {
+                         System.out.println("Помилкові дані! Має бути число від 0 до 10! Спробуй ще раз.");
+                         sc.nextLine();
+                    }
+                    if (num != random && (num >= 0 && num <= 10)) {
+                         System.out.println("Вибач, але ти не вгадав. :( \nСпробуй ще раз: ");
+                         sc.nextLine();
+                    } else if (num == random) {
+                         System.out.println("Вітаю! Ти вгадав!");
+                         System.out.println("Загадане число було " + random + ".");
+                         break;
+                    }
+               } else {
+                    System.out.println("Помилкові дані! Має бути число від 0 до 10! Спробуй ще раз.");
+                    sc.nextLine();
+               }
+          }
+     }
+}
+
+
+
+
