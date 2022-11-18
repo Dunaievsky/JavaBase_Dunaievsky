4bd288ad7a96172c1d152bd8d1bb07cb81bffdc1
Dunaievsky Vladyslav <vladunaevsky@gmal.com>
2022-11-18 11:33:21 +0300

    commit Lottery

---
+++ src/main/java/com/hillel/dunaievsky/homeworks/homework8/MyApp8.java
@@ -0,0 +1,49 @@
+package com.hillel.dunaievsky.homeworks.homework8;
+
+import java.util.Arrays;
+
+public class MyApp8 {
+
+
+    public static void main(String[] args) {
+
+        int n = 7;
+        int count = 0;
+
+        int [] arrCompanyNumbers = array(n);
+        int [] arrUserNumbers = array(n);
+
+        Arrays.sort(arrCompanyNumbers);
+        Arrays.sort(arrUserNumbers);
+
+        System.out.println(Arrays.toString(arrCompanyNumbers) + " числа, які загадані фірмою-організатором лотереї.");
+        System.out.println(Arrays.toString(arrUserNumbers) + " числа, які вказав гравець.");
+        System.out.println();
+        System.out.println("Збіг в позиціях: ");
+
+        for (int i = 0; i < n; i++) {
+            if (arrCompanyNumbers[i] == arrUserNumbers[i]) {
+
+                System.out.print(i + " ");
+                count++;
+
+            }
+        }
+        if (count == 0) {
+            System.out.println("Не знайдено");
+        }
+        System.out.println();
+        System.out.println("Кількість збігів: " + count);
+    }
+
+    public static int[] array(int n) {
+
+        int [] array = new int [n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = (int) (Math.random() * 10);
+        }
+        return array;
+    }
+
+}
