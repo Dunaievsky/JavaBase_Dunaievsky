3d6922e37eb5a52658217539a094a286632e6bd5
Dunaievsky Vladyslav <vladunaevsky@gmal.com>
2022-12-11 18:52:21 +0300

    commit Burger

---
+++ src/main/java/com/hillel/dunaievsky/homeworks/homework12/Burger.java
@@ -0,0 +1,59 @@
+package com.hillel.dunaievsky.homeworks.homework12;
+
+public class Burger {
+
+    String bun;
+    String meat;
+    String cheese;
+    String lettuce;
+    String mayonese;
+
+    public Burger(String bun, String meat, String cheese, String greens, String mayo) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.lettuce = greens;
+        this.mayonese = mayo;
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Burger 'full' consists of:" +
+                " " + bun +
+                ", " + meat +
+                ", " + cheese +
+                ", " + lettuce +
+                ", " + mayonese +
+                '.';
+    }
+
+    public Burger(String bun, String meat, String cheese, String greens) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.lettuce = greens;
+
+        printInfo();
+    }
+
+    void printInfo() {
+        System.out.println("Burger 'dietetic' consists of: " + bun + ", " + meat
+                + ", " + cheese + ", " + lettuce + ".");
+    }
+
+
+    public Burger(String bun, String meat, String cheese, String greens, String mayo, boolean isDouble) {
+        this.bun = bun;
+        this.meat = isDouble ? "doubleMeat" : meat;
+        this.cheese = cheese;
+        this.lettuce = greens;
+        this.mayonese = mayo;
+
+        System.out.println("Burger 'doubleMeat' consists of: " + this.bun + ", " +
+                this.meat + ", " + this.cheese + ", " + this.lettuce + ", " + this.mayonese + ".");
+    }
+}
+
+
+
