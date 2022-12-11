6aabedab6207dbd9b61b70a1266dc0a475340c6e
Dunaievsky Vladyslav <vladunaevsky@gmal.com>
2022-12-12 01:29:37 +0300

    commit MyApp15

---
+++ src/main/java/com/hillel/dunaievsky/homeworks/homework13/Main.java
@@ -0,0 +1,28 @@
+package com.hillel.dunaievsky.homeworks.homework13;
+
+public class Main {
+    public static void main(String[] args) {
+        Androids androids = new Androids();
+        androids.sms();
+        androids.call();
+        androids.internet();
+        androids.LinuxOS();
+        System.out.println();
+
+        iPhones iPhones = new iPhones();
+        iPhones.call();
+        iPhones.internet();
+        iPhones.sms();
+        iPhones.IOS();
+
+
+
+
+
+
+
+    }
+}
+
+
+
