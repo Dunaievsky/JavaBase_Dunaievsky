2ce1af8a1b2d11f7abf412d952e14566a84df363
Dunaievsky Vladyslav <vladunaevsky@gmal.com>
2022-12-21 19:04:53 +0300

    commit MyApp18

--- src/main/java/com/hillel/dunaievsky/homeworks/homework16/Drinks.java
+++ src/main/java/com/hillel/dunaievsky/homeworks/homework16/Drinks.java
@@ -1,12 +1,12 @@
 package com.hillel.dunaievsky.homeworks.homework16;
 
 public class Drinks {
-    public static final int COFFEE_PRICE = 20;
-    public static final int TEA_PRICE = 10;
-    public static final int LEMONADE_PRICE = 15;
-    public static final int MOJITO_PRICE = 40;
-    public static final int WATER_PRICE = 12;
-    public static final int COLA_PRICE = 30;
+    public static final int COFFEE_PRICE = 36;
+    public static final int TEA_PRICE = 22;
+    public static final int LEMONADE_PRICE = 20;
+    public static final int MOJITO_PRICE = 80;
+    public static final int WATER_PRICE = 15;
+    public static final int COLA_PRICE = 22;
 
 }
 
