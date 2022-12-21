ea5f7e7899fa3c099db46c50c712e3983fca696a
Dunaievsky Vladyslav <vladunaevsky@gmal.com>
2022-12-21 19:03:35 +0300

    commit MyApp18

---
+++ src/main/java/com/hillel/dunaievsky/homeworks/homework16/Drink.java
@@ -0,0 +1,10 @@
+package com.hillel.dunaievsky.homeworks.homework16;
+
+public class Drink {
+    public static int count = 0;
+
+    public Drink() {
+        count++;
+    }
+}
+
---
+++ src/main/java/com/hillel/dunaievsky/homeworks/homework16/DrinksMachine.java
@@ -0,0 +1,22 @@
+package com.hillel.dunaievsky.homeworks.homework16;
+
+public enum DrinksMachine {
+    TEA("Чай"),
+    COFFEE("Кава"),
+    LEMONADE("Лимонад"),
+    MOJITO("Мохіто"),
+    MINERAL_WATER("Мінеральна вода"),
+    COCA_COLA("Кока-Кола");
+    private String title;
+
+    DrinksMachine(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
+
+
